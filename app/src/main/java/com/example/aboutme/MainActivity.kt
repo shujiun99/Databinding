886696a609e5619a6b5f016d7e12f4cdccae93f5
shujiun99 <leesj-wm18@student.tarc.edu.my>
2021-07-05 18:09:44 +0800

    Initial

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.aboutme
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val aName : PersonName = PersonName("ShuJiun","Haha")
+        binding.myName = aName
+
+        binding.changebutton.setOnClickListener{
+            aName.nickName = binding.editTextTextPersonName2.text.toString()
+            binding.changebutton.visibility = View.GONE
+            binding.editTextTextPersonName2.visibility = View.GONE
+            binding.textView3.text = binding.editTextTextPersonName2.text
+            binding.textView3.visibility = View.VISIBLE
+        }
+
     }
 }
